5431a86907a7ed6b87e705fb030507ad7a0e8834
Enfec Technologies <Enfec Technologies@DESKTOP-E8M51J5>
2020-02-25 16:12:38 +0530

    initial

---
+++ Ref_Booking_Status/src/main/java/com/example/demo/controller/BookingController.java
@@ -0,0 +1,107 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.Bookingdao;
+import com.example.demo.model.Booking;
+
+import com.google.gson.Gson;
+
+@RestController
+public class BookingController {
+	private static final Logger logger = LoggerFactory.getLogger(BookingController.class);
+	@Autowired
+	private Bookingdao dao;
+
+	@GetMapping(value = "/getDetails/{booking_status_code}", produces = "application/json;charset=UTF-8")
+	public ResponseEntity<String> get(@PathVariable("booking_status_code") Integer booking_status_code) {
+		try {
+			List<Booking> book = dao.getBookingDetails(booking_status_code);
+			if (book.isEmpty()) {
+				logger.info("No data found for: {} ", booking_status_code);
+				return new ResponseEntity<>("{\"message\" : \"No Details found\"}", HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(new Gson().toJson((dao.getBookingDetails(booking_status_code))),
+						HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			logger.error("Exception in getting  info: {} ", e.getMessage());
+			return new ResponseEntity<>("{\"message\" : \"Exception in getting  info\"}",
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	@PostMapping(value = "/save")
+	public ResponseEntity<String> save(@RequestBody(required = true) Booking b) {
+		try {
+			int affectedRow = dao.saveBooking(b);
+
+			if (affectedRow == 0) {
+				logger.info("getBooking_status_code: {} ", b.getBooking_status_code());
+				return new ResponseEntity<>("{\"message\" : \"Invalid Deatils\"}", HttpStatus.OK);
+			} else {
+				logger.info("Details Entered getBooking_status_code: {} ", b.getBooking_status_code());
+				return new ResponseEntity<>("{\"message\" : \"Details Successfully Entered \"}", HttpStatus.OK);
+			}
+
+		} catch (DataIntegrityViolationException dataIntegrityViolationException) {
+			logger.error("Invalid getBooking_status_code: {} ", b.getBooking_status_code());
+			return new ResponseEntity<>("{\"message\" : \"Invalid getBooking_status_code\"}", HttpStatus.BAD_REQUEST);
+		} catch (Exception exception) {
+			logger.error("Exception in Booked  :{}", exception.getMessage());
+			return new ResponseEntity<>("{\"message\" : \"Exception in Booking  info\"}",
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping(value = "/update")
+	public ResponseEntity<String> put(@RequestBody(required = true) Booking b) {
+		try {
+			int affectedRow = dao.updateBooking(b);
+
+			if (affectedRow == 0) {
+				logger.info("getBooking_status_code: {} ", b.getBooking_status_code());
+				return new ResponseEntity<>("{\"message\" : \"Invalid Deatils\"}", HttpStatus.OK);
+			} else {
+				logger.info("Details Entered getBooking_status_code: {} ", b.getBooking_status_code());
+				return new ResponseEntity<>("{\"message\" : \"Details Updated Successfully Entered \"}", HttpStatus.OK);
+			}
+
+		} catch (DataIntegrityViolationException dataIntegrityViolationException) {
+			logger.error("Invalid getBooking_status_code: {} ", b.getBooking_status_code());
+			return new ResponseEntity<>("{\"message\" : \"Invalid Booking_status_code\"}", HttpStatus.BAD_REQUEST);
+		} catch (Exception exception) {
+			logger.error("Exception in Booked  :{}", exception.getMessage());
+			return new ResponseEntity<>("{\"message\" : \"Exception in Booking  info\"}",
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping(value = "/deleteBooking/{booking_staus_code}", produces = "application/json;charset=UTF-8")
+	public ResponseEntity<String> delete(@PathVariable("booking_staus_code") int booking_staus_code) {
+		int rows = dao.deleteBooking(booking_staus_code);
+		if (rows > 0)
+			return new ResponseEntity<>("{\"message\" : \"booking_staus_code Details deleted\"}", HttpStatus.OK);
+
+		else
+			return new ResponseEntity<>("{\"message\" : \"booking_staus_code not found\"}", HttpStatus.BAD_REQUEST);
+
+	}
+
+}
---
+++ Ref_Booking_Status/src/main/java/com/example/demo/model/Booking.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+	private int booking_status_code;
+	private String booking_status_description;
+
+}
